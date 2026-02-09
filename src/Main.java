1110abef868f3b37ed275f76d0332791c2761778
Marc <marcsegura@paucasesnovescifp.cat>
2026-02-09 18:01:25 +0100

    push fet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(1);
+
+
+        int suma = suma(2, 5);
     }
+
+    static void suma(int a, int b) {
+        return a + b;
+    }
+
 }
