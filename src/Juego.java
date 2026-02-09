d4ed5639c1353b0c737338073292e1f62863bc03
Noaaaah07 <noahamengualvarela@gmail.com>
2026-02-09 18:14:43 +0100

    Noah

--- src/Juego.java
+++ src/Juego.java
@@ -6,7 +6,7 @@ public class Juego {
         int hp;
         int maxHp;
         int ataque;
-
+        
         public Pokemon(String nombre, int hp, int ataque) {
             this.nombre = nombre;
             this.hp = hp;
